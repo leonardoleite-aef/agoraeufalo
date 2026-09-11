62f945136b111ce056cf60f6451034f92fbb66f5
leonardoleite-aef <selexenglish@gmail.com>
2026-09-11 01:36:33 -0300

    chore: remove test files

--- patch_player.js
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-let code = fs.readFileSync('player.html', 'utf8');
-code = code.replace(
-  'hierarchy = await syncEngine.getCoursesHierarchy(window.AEF_COURSES_REGISTRY || null);',
-  'hierarchy = await syncEngine.getCoursesHierarchy(window.AEF_COURSES_REGISTRY || null); console.log("getCoursesHierarchy returned", hierarchy);'
-);
-fs.writeFileSync('player.html', code);
